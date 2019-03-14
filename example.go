627cdbef7b9312a2eb1ca6446a8ad197b50eb415
watanata <watanabetatsuya@lifull.com>
2019-03-14 09:53:40 +0900

    add path to return sample json data

--- example.go
+++ example.go
@@ -1,6 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SampleData struct {
+	Hoge string  `json:"hoge"`
+	Foo  MapData `json:"foo"`
+}
+
+type MapData struct {
+	Bar  string `json:"bar"`
+	Fuga FugaData
+}
+
+type FugaData struct {
+	Piyo string `json:"piyo"`
+}
 
 func main() {
 	r := gin.Default()
@@ -8,6 +28,21 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		data, err := ioutil.ReadFile("./data.json")
+		if err != nil {
+			fmt.Print(err)
+		}
+
+		var obj SampleData
+		err = json.Unmarshal(data, &obj)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		c.JSON(200, obj)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
